eb06c6a6c9c0e9d375feab2b29915c0e14dce6cf
wendellvian <ybyqlong@126.com>
2014-11-27 17:58:02 +0800

    update

--- _site/js/webql.animate.queue.js
+++ _site/js/webql.animate.queue.js
@@ -9,17 +9,44 @@ webql.animateMove = function (arr){
 			setTimeout(callback,1000/60);
 		};
 		var lis = arr || [];
+		var getNow = function(){
+			return Date.now ? Date.now() : (new Date()).getTime();
+		}
+		// 队列转换执行时间方法
+		var lisInit = function(){
+			var timei = 0;
+			for(var i=0;i<lis.length;i++){
+				lis[i].time = timei + lis[i].exetime;
+				timei = lis[i].time;
+			}
+		}();
+		var lastTickTime = getNow();
+
 		(function tick(){
+			var tickTime = getNow() - lastTickTime;
+			lastTickTime = getNow();
 			for(var i=lis.length-1;i>=0;i--){
-				lis[i].time -= 1000/60;
+				lis[i].time -= tickTime;
 				if(lis[i].time <= 0){
-					lis[i].fn();
-					lis.splice(i,1);
+					for(var j=lis[i].queue.length-1;j>=0;j--){
+						lis[i].queue[j].time -= tickTime;
+						if(lis[i].queue[j].time <= 0){
+							lis[i].queue[j].fn.apply(null,lis[i].queue[j].param);
+							// 每执行完一条动画就干掉它
+							lis[i].queue.splice(j,1);
+						}
+					}
+					if(lis[i].queue.length == 0){
+						lis.splice(i,1);
+					}
 				}
 			}
-			requestAnimationFrame(tick);
+			if(lis.length>0){
+				requestAnimationFrame(tick);
+			}
 		})();
+
 	};
-webql.tabClass = function (ele, ClassName){
-	ele.addClass(ClassName);
+webql.tabClass = function (obj, ClassName){
+	obj.addClass(ClassName);
 }
